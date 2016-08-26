fa76f93c723781477a5a618bf23efdeeac8a813a
GH <gerald.holmes@outlook.com>
2016-08-25 23:05:46 -0400

    Testing small change

--- app/src/main/java/com/bytepair/stormy/MainActivity.java
+++ app/src/main/java/com/bytepair/stormy/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // bind butterknife to this
+        // bind ButterKnife to this
         ButterKnife.bind(this);
 
         // hide progress bar when page loads
